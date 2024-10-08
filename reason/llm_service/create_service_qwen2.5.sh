05974e13ee95f4324d9c1901e6d45d66d9735f2f
iamlilAJ <98063079+iamlilAJ@users.noreply.github.com>
2024-10-08 17:07:08 +0800

    Update create_service_qwen2.5.sh (#1)

--- reason/llm_service/create_service_qwen2.5.sh
+++ reason/llm_service/create_service_qwen2.5.sh
@@ -8,24 +8,25 @@ echo PYTHON_EXECUTABLE=$(which python3)
 PYTHON_EXECUTABLE=$(which python3)
 
 MODEL_BASE=/hpc2ssd/JH_DATA/spooler/qxiao183/workspace/hf_models/
-CUDA_DEVICE_BASE=1
-POLICY_MODEL_NAME=Qwen/Qwen2.5-Math-1.5B-Instruct
-# VALUE_MODEL_NAME=Qwen/Qwen2.5-Math-7B-Instruct
+CUDA_DEVICE_BASE=0
+POLICY_MODEL_NAME=peiyi9979/mistral-7b-sft
+#VALUE_MODEL_NAME=peiyi9979/math-shepherd-mistral-7b-prm
+VALUE_MODEL_NAME=Qwen/Qwen2.5-Math-7B-Instruct
 MODEL_PATH=$MODEL_BASE/$POLICY_MODEL_NAME
 VALUE_MODEL_PATH=$MODEL_BASE/$VALUE_MODEL_NAME
 
 LOGDIR=logs_fastchat
 
 tmux start-server
-tmux new-session -s FastChat1 -n controller -d
+tmux new-session -s FastChat2 -n controller -d
 tmux send-keys "export LOGDIR=${LOGDIR}" Enter
 tmux send-keys "$PYTHON_EXECUTABLE -m fastchat.serve.controller --port ${CONTROLER_PORT} --host $HOST_ADDR" Enter
 
-NUM_LM_WORKER=2
-NUM_RM_WORKER=0
+NUM_LM_WORKER=1
+NUM_RM_WORKER=1
 
 echo "Wait 10 seconds ..."
-sleep 5
+sleep 2
 
 echo "Starting workers"
 for i in $(seq 0 $((NUM_LM_WORKER-1)))
@@ -43,5 +44,5 @@ do
   WORKER_PORT=$((i+WORKER_BASE_PORT+NUM_LM_WORKER))
   tmux new-window -n value_worker
   tmux send-keys "export LOGDIR=${LOGDIR}" Enter
-  tmux send-keys "CUDA_VISIBLE_DEVICES=$((i+NUM_LM_WORKER+CUDA_DEVICE_BASE)) $PYTHON_EXECUTABLE -m reason.llm_service.math_shepherd_model_worker --model-path $VALUE_MODEL_PATH --controller-address http://$HOST_ADDR:$CONTROLER_PORT --host $HOST_ADDR --port $WORKER_PORT --worker-address http://$HOST_ADDR:$WORKER_PORT" Enter
+  tmux send-keys "CUDA_VISIBLE_DEVICES=$((i++NUM_LM_WORKER+CUDA_DEVICE_BASE)) $PYTHON_EXECUTABLE -m reason.llm_service.qwen_model_worker --model-path $VALUE_MODEL_PATH --controller-address http://$HOST_ADDR:$CONTROLER_PORT --host $HOST_ADDR --port $WORKER_PORT --worker-address http://$HOST_ADDR:$WORKER_PORT" Enter
 done
