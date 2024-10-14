432ab23734af272aae98dd0618096cf707b89be8
Gebro13 <80039181+Gebro13@users.noreply.github.com>
2024-10-14 14:15:08 +0800

    Add files via upload
    (cherry picked from commit dbfcf7e633d003b3448eeef448456a419c863432)

--- prm/offline_eval.py
+++ prm/offline_eval.py
@@ -3,12 +3,14 @@ import tree
 import numpy as np
 import sys
 from reason.evaluation.evaluator import judge_ans
-from reason.inference.rm_call import RMRemoteCaller
+from reason.inference.rm_call import RMRemoteCaller, LocalCaller
+# from prm.infer_fns import LocalCaller
 from reason.evaluation.evaluator import Task
 from reason.reranking.vote_utils import AGG_FN_MAP
 from tqdm import tqdm
 import jsonlines
 from pathlib import Path
+import copy
 
 
 def main(record_path, prm_and_tags):
@@ -20,13 +22,15 @@ def main(record_path, prm_and_tags):
     # ms_rm = RMRemoteCaller("math-shepherd-mistral-7b-prm", step_tag='ки\n')
     # qw_rm = RMRemoteCaller("Qwen2.5-Math-7B-PRM", step_tag='\n\n\n\n\n')
 
-   
+    prm_new_outputs_dict = {}
     prm_result_collect = {}
     for prm_name, step_tag in prm_and_tags:
-        rm_call = RMRemoteCaller(prm_name, step_tag=step_tag)
+        rm_call = LocalCaller(prm_name, step_tag=step_tag)
         res_list = []
         orig_res_list = []
+        new_output_list = []
         for output in tqdm(my_outputs):
+            new_output = copy.deepcopy(output)
             question = output['question']
             groundtruth = output['groundtruth']
 
@@ -43,6 +47,8 @@ def main(record_path, prm_and_tags):
             # print(rm_inputs[0])
             # break
             rm_v = rm_call(rm_inputs)
+            for n_out, new_v in zip(new_output['output'], rm_v):
+                n_out['value'] = new_v
             assert len(rm_v) == len(original_v)
             res = {
                 k: 
@@ -78,21 +84,35 @@ def main(record_path, prm_and_tags):
                 'value': rm_v,
                 'result': res
             }
+            new_output_list.append(new_output)
         prm_result_collect[prm_name] = tree.map_structure(lambda *xs: np.mean(xs), *res_list)
         orig_res = tree.map_structure(lambda *xs: np.mean(xs), *orig_res_list)
         # print(orig_res)
         print("PRM {}: {}\nORIG: {}".format(prm_name, prm_result_collect[prm_name], orig_res))
+        prm_new_outputs_dict[prm_name] = new_output_list
 
 
     print(prm_result_collect)
+
+    for prm_name, new_objs in prm_new_outputs_dict.items():
+        with jsonlines.open(f"new_record_{prm_name.split('/')[-1]}.jsonl", 'w') as f:
+            f.write_all(new_objs)
 
 if __name__ == "__main__":
     # HOW TO USE
     # First deploy your PRMs and then set `step_tags` here
     # You can also modify the code to run the prm locally.
+    # prm_and_tags = [
+    #     # ("math-shepherd-mistral-7b-prm", 'ки\n'),
+    #     ("/mnt/nfs1/workspace/o1-dev/prm/prm_results_qwen_new.31/bs_256_lr_0.0001_datasets_aps_shepherd/checkpoint-2331", '\n\n\n\n\n'),
+    # ]
     prm_and_tags = [
         # ("math-shepherd-mistral-7b-prm", 'ки\n'),
-        ("checkpoint-6898", '\n\n\n\n\n'),
+        ("/mnt/nfs1/workspace/o1-dev/prm/prm_results_qwen_new.31/bs_256_lr_0.0001_datasets_all/checkpoint-2127", '\n\n\n\n\n'),
     ]
+    # prm_and_tags = [
+    #     # ("math-shepherd-mistral-7b-prm", 'ки\n'),
+    #     ("/mnt/nfs1/workspace/o1-dev/prm/prm_results_qwen_new.1/bs_256_lr_0.0001_datasets_math_aps/checkpoint-595", '\n\n\n\n\n'),
+    # ]
 
     main(Path(__file__).parent / 'record.jsonl', prm_and_tags)
