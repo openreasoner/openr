06b8637898e67b428edaab2c4266c9275b80fe84
ziyuwan <alex_wan@sjtu.edu.cn>
2024-10-23 17:57:00 +0800

    fix typo in scripts

--- reason/llm_service/create_service_qwen2.5_math_hf.sh
+++ reason/llm_service/create_service_qwen2.5_math_hf.sh
@@ -44,5 +44,5 @@ do
   WORKER_PORT=$((i+WORKER_BASE_PORT+NUM_LM_WORKER))
   tmux new-window -n value_worker
   tmux send-keys "export LOGDIR=${LOGDIR}" Enter
-  tmux send-keys "CUDA_VISIBLE_DEVICES=$((i++NUM_LM_WORKER+CUDA_DEVICE_BASE)) $PYTHON_EXECUTABLE -m reason.llm_service.workers.reward_model_worker --model-path $VALUE_MODEL_PATH --controller-address http://$HOST_ADDR:$CONTROLER_PORT --host $HOST_ADDR --port $WORKER_PORT --worker-address http://$HOST_ADDR:$WORKER_PORT" Enter
+  tmux send-keys "CUDA_VISIBLE_DEVICES=$((i+NUM_LM_WORKER+CUDA_DEVICE_BASE)) $PYTHON_EXECUTABLE -m reason.llm_service.workers.reward_model_worker --model-path $VALUE_MODEL_PATH --controller-address http://$HOST_ADDR:$CONTROLER_PORT --host $HOST_ADDR --port $WORKER_PORT --worker-address http://$HOST_ADDR:$WORKER_PORT" Enter
 done
