28954965c8e643949900287a135fc9e138d47fda
Ziqin Gong <gongzq0301@gmail.com>
2024-11-12 16:12:12 +0800

    read math-aps-v2-tree

--- preprocess/src/preprocessors/utils.py
+++ preprocess/src/preprocessors/utils.py
@@ -2,7 +2,13 @@ import json
 from pathlib import Path
 from typing import Iterable
 
-from src.data_types import ConvertedItem, MathAPSItem, MathShepherdItem, PRM800KItem
+from src.data_types import (
+    ConvertedItem,
+    MathAPSItem,
+    MathAPSItemV2Tree,
+    MathShepherdItem,
+    PRM800KItem,
+)
 
 
 def read_prm800k_ds(ds_path: Path) -> Iterable[PRM800KItem]:
@@ -24,6 +30,13 @@ def read_math_shepherd_ds(ds_path: Path) -> Iterable[MathShepherdItem]:
         dict_lst = map(json.loads, fd.readlines())
 
     return map(MathShepherdItem.from_dict, dict_lst)
+
+
+def read_math_aps_v2_tree_ds(ds_path: Path) -> Iterable[MathAPSItemV2Tree]:
+    with open(ds_path, "r", encoding="utf-8") as fd:
+        dict_lst = map(json.loads, fd.readlines())
+
+    return map(MathAPSItemV2Tree.from_dict, dict_lst)
 
 
 def dump_converted_ds(save_path: Path, items: list[ConvertedItem]) -> None:
