212006ca30512bcb6501806fa65942f21ef82631
Ziqin Gong <gongzq0301@gmail.com>
2024-11-12 14:46:49 +0800

    math-aps-v2 data types

--- preprocess/tests/test_data_types.py
+++ preprocess/tests/test_data_types.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 import pytest
-from src.data_types.math_aps import MathAPSItem
+from src.data_types.math_aps import MathAPSItem, MathAPSItemV2, MathAPSItemV2Tree
 from src.data_types.math_shepherd import MathShepherdItem
 from src.data_types.prm800k import PRM800KItem
 
@@ -24,6 +24,16 @@ def example_math_shepherd_path() -> Path:
     return TEST_DIR / "samples_math-shepherd.jsonl"
 
 
+@pytest.fixture(scope="module")
+def example_math_aps_v2_tree_path() -> Path:
+    return TEST_DIR / "samples_math-aps-v2_tree.jsonl"
+
+
+@pytest.fixture(scope="module")
+def example_math_aps_v2_path() -> Path:
+    return TEST_DIR / "samples_math-aps-v2.jsonl"
+
+
 def test_prm800k_item(example_prm800k_path: Path) -> None:
     with open(example_prm800k_path, "r") as fd:
         item = PRM800KItem.from_dict(json.loads(fd.readline()))
@@ -43,6 +53,22 @@ def test_math_aps_item(example_math_aps_path: Path) -> None:
 def test_math_shepherd_item(example_math_shepherd_path: Path) -> None:
     with open(example_math_shepherd_path, "r") as fd:
         item = MathShepherdItem.from_dict(json.loads(fd.readline()))
+
+    print(item)
+    assert True
+
+
+def test_math_aps_item_v2_tree(example_math_aps_v2_tree_path: Path) -> None:
+    with open(example_math_aps_v2_tree_path, "r") as fd:
+        item = MathAPSItemV2Tree.from_dict(json.loads(fd.readline()))
+
+    print(item)
+    assert True
+
+
+def test_math_aps_item_v2(example_math_aps_v2_path: Path) -> None:
+    with open(example_math_aps_v2_path, "r") as fd:
+        item = MathAPSItemV2.from_dict(json.loads(fd.readline()))
 
     print(item)
     assert True
